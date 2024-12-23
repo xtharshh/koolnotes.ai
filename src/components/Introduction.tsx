f46d63809ba0a6bccafb6e5df66579819a8838c6
Harsh Kumar <121563719+xtharshh@users.noreply.github.com>
2024-12-24 02:07:03 +0530

    Update Introduction.tsx

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Introduction = () => {
     return (
-        <div className="pb-20 bg-eclipse bg-cover bg-center" >
+        <div className="pb-20 bg-newEclipse bg-cover bg-center" >
             <main className="p-20">
                 <div className=" p-5 rounded-lg  text-center ">
                     <h1 className="text-yellow-400 py-20 pb-0 text-5xl font-bold font-new ">
@@ -17,7 +17,7 @@ const Introduction = () => {
                         </span>
                         </h2>
                     <Button/>
-                    <p className="text-red-500 font-bold font-cursive text-4xl py-10">
+                    <p className="text-red-500 font-bold font-newGab text-4xl py-10">
                     <span className=' p-2'>Discover the amazing features we offer to enhance your study.</span></p>
                 </div>
             </main>
