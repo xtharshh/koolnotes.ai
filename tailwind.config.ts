95a8da6d4b353fa7a0268b6ef283f17e0e7157c5
Harsh Kumar <121563719+xtharshh@users.noreply.github.com>
2024-12-24 01:52:40 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   ],
   theme: {
     extend: {
-      backgroundImage: () => ({ 'eclipse': "url('https://res.cloudinary.com/djcbdfehg/image/upload/v1734984686/downloadfile_xdoat0.jpg')", }),
+      backgroundImage: () => ({ 'eclipse': "url('https://res.cloudinary.com/djcbdfehg/image/upload/v1734984686/downloadfile-1_apro5p.jpg')", }),
       colors: { 
         customBeige: 'rgba(156, 132, 73, 0.9)',
         background: "var(--background)",
