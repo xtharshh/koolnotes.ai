7849c8159b7150fd7a1fb777987fe03a1638300a
HARSH KUMAR <xt.harshh@gmail.com>
2024-12-25 18:14:21 +0530

    fixed some issues new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -158,7 +158,7 @@ const Footer: React.FC = () => {
         </div>
         <hr className="my-6 border-blueGray-300" />
         <div className="flex flex-wrap items-center md:justify-between justify-center">
-          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+          <div className="w-full  px-4 mx-auto text-center">
             <div className="text-blueGray-500 text-xl py-1 font-newGab font-bold">
               ©{new Date().getFullYear()} Made With ❤️ by&nbsp;
               <a
@@ -167,7 +167,7 @@ const Footer: React.FC = () => {
               >
                 Harsh
               </a>
-              <span>&nbsp;&</span>
+              <span>&nbsp;&&nbsp;</span>
               <a
                 href="https://www.instagram.com/yash._.deep"
                 className="text-blueGray-500 hover:text-gray-500"
