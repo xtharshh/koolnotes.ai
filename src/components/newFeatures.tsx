31839542ba9a2dcfcf02ae2e85615cf40c8b7afe
HARSH KUMAR <xt.harshh@gmail.com>
2024-12-25 18:49:26 +0530

    fixed some issues new

--- src/components/newFeatures.tsx
+++ src/components/newFeatures.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-import FeatureCard from "./ui/featureCard";
+import React from 'react';
+import FeatureCard from './ui/featureCard';
 
 const NewFeatures = React.memo(() => {
   return (
-    <>
-      <div className="bg-white dark:bg-black text-black dark:text-white">
-        <h1 className="text-4xl font-bold font-newLuck text-center pt-10">New Features</h1>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xlg:grid-cols-4 gap-4 p-16 pt-10 pb-24">
-            <FeatureCard />
-            <FeatureCard />
-            <FeatureCard />
-            <FeatureCard />
-          </div>
+    <div className="bg-white dark:bg-black text-black dark:text-white">
+      <h1 className="text-4xl font-bold font-newLuck text-center pt-10">New Features</h1>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xlg:grid-cols-4 gap-4 p-16 pt-10 pb-24">
+          <FeatureCard name="Jobs & Internships" />
+          <FeatureCard name="Ai Summary" />
+          <FeatureCard name="Upload & Earn" />
+          <FeatureCard name="PixelWalls" />
         </div>
       </div>
-    </>
+    </div>
   );
 });
 
-NewFeatures.displayName = "NewFeatures";
+NewFeatures.displayName = 'NewFeatures';
 
 export default NewFeatures;
