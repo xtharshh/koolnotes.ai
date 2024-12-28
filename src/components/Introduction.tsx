63105e6d69b7f33e373b876e84f50109d0f105ef
HARSH KUMAR <xt.harshh@gmail.com>
2024-12-28 23:13:58 +0530

    improved images for college

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,14 +1,14 @@
 "use client";
 import Button from './ui/dualBut';
 import React from 'react';
-import TypingAnimation from "../components/ui/typing-animation";
+
 const Introduction: React.FC = React.memo(() => {
   return (
     <div id='introduction' className="pb-20  dark:bg-background bg-cover bg-center bg-white dark:bg-black text-black dark:text-white">
       <main className="p-20">
         <div className="p-5 rounded-lg text-center">
           <h1 className="dark:text-yellow-400 text-black py-20 pb-0 text-5xl font-bold font-newLuck">
-            <TypingAnimation>Welcome to KoolNotes</TypingAnimation>
+            <a className='koolnotes'>KoolNotes</a>
           </h1>
           <h2 className="dark:text-green-100 text-blue-600 py-6 pb-0 text-4xl font-newLuck font-bold mb-2">
             <span>Access study notes, past papers, AI summaries, and chat support for all subjects and semesters. We&apos;re continuously expanding with new colleges and materials.
